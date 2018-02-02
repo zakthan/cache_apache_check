5a75ee744df08525203704877dc1b18de9b58d4a
Thanassis Zakopoulos <zakthan@gmail.com>
2018-02-02 16:13:51 +0200

    Update cache_chech.sh
    changed $date to keep double spaces when date strts with 0 (for example Feb 02)

--- cache_chech.sh
+++ cache_chech.sh
@@ -2,18 +2,21 @@
 HOST=`hostname`
 ZABBIX=pvzabbix01.cosmote.gr
 KEY=cache_count_for_current_date
-MAILLIST=middleware@ote.gr
+##MAILLIST=middleware@ote.gr
+MAILLIST=azakopoulos@ote.gr
 date=`date '+%b %d'`
+##if data is 0? then remove 0
+#####date=$(echo $date|sed "s/0/ /")
 cache_root=/tmp/httpd/cache/
-COUNT_FILES_WITH_CURRENT_DATE=$(find $cache_root  -type f|xargs ls -lt|head|grep "$(echo $date)"|wc -l)
+COUNT_FILES_WITH_CURRENT_DATE=$(find $cache_root  -type f|xargs ls -lt|head|grep "$(echo $date|sed "s/0/ /")"|wc -l)
 ####echo $COUNT_FILES_WITH_CURRENT_DATE
 
 /usr/bin/zabbix_sender -z $ZABBIX -s $HOST -k $KEY -o $COUNT_FILES_WITH_CURRENT_DATE
 
 if [ $COUNT_FILES_WITH_CURRENT_DATE -eq 0 ]
 then 
-##	echo 0
-	mailx -s "check apache cache validity for $HOST" $MAILLIST <.
-##else 	
-##	echo 1
+##      echo 0
+        mailx -s "check apache cache validity for $HOST" $MAILLIST <.
+##else 
+##      echo 1
 fi
